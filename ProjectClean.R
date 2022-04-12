61e050a323b7e3ef14948bd184fd8fae88aed413
iancummings <icummings@luc.edu>
2022-04-12 15:52:37 -0500

    ian

--- ProjectClean.R
+++ ProjectClean.R
@@ -4,7 +4,7 @@ library(lubridate)
 library(plyr) 
 library(eeptools)
 library(dplyr)
-data <- read.csv("/Users/corinnesteuk/Documents/STAT310/VehicleLoanDefault.csv", sep = ',')
+data <- read.csv("VehicleLoanDefault.csv", sep = ',')
 head(data)
 
 
@@ -48,55 +48,22 @@ dis <- as.list(scan(text=d$DisbursalDate, what=" "))
 dis <- dmy(dis)
 data['Disbursed.Time.Months'] <- age_calc(dis,  units = 'months')
 head(data)
-=======
-data <- read.csv("Train_Dataset.csv", sep = ",")
-head(data)
 
-#checking how many NAs there are
-sum(is.na(data))
-#264134
+###AVG ACCOUNT AGE & CREDIT HISTORY LENGTH TO LENGTH IN MONTHS
+#Credit history length first:
+data$CREDIT.HISTORY.LENGTH <- regmatches(data$CREDIT.HISTORY.LENGTH, gregexpr("[[:digit:]]+",data$CREDIT.HISTORY.LENGTH))
 
-#well, that's not ideal
-#some are just empty strings
-sum(is.na(data$Registration_Days))
-sum(data$Registration_Days == '')
-typeof(data$Registration_Days[3])
-typeof(data$Client_Occupation[3])
-data$Score_Source_1[2]
-
-#replacing empty strings w/ NAs
-n_rows <- dim(data)[1]
-n_columns <- dim(data)[2]
-for (i in 1:n_columns){
-  data[,i][data[,i] == ''] <- NA
+to_date_in_months <- function(x){
+  as.numeric(unlist(x))[1] * 12 + as.numeric(unlist(x))[2]
 }
 
-#Changing columns of numbers that were originally characters 
-#to type integer or double (so NA's will populate)
-options(digits = 10)
-data$Client_Income <- as.integer(data$Client_Income)
-data$Credit_Amount <- as.numeric(data$Credit_Amount)
-data$Loan_Annuity <- as.numeric(data$Loan_Annuity)
-data$Population_Region_Relative <- as.numeric(data$Population_Region_Relative)
-data$Age_Days <- as.numeric(data$Age_Days)
-data$Employed_Days <- as.numeric(data$Employed_Days)
-data$Registration_Days <- as.numeric(data$Registration_Days)
-data$ID_Days <- as.numeric(data$ID_Days)
-data$Application_Process_Hour <- as.numeric(data$Application_Process_Hour)
-data$Score_Source_3 <- as.numeric(data$Score_Source_3)
+data$CREDIT.HISTORY.LENGTH <- lapply(data$CREDIT.HISTORY.LENGTH, to_date_in_months)
 
-#checking NAs again
-sum(is.na(data))
-#395811
+#Now Account Age:
+data$AVERAGE.ACCT.AGE <- regmatches(data$AVERAGE.ACCT.AGE, gregexpr("[[:digit:]]+", data$AVERAGE.ACCT.AGE))
 
-column_nas <- rep(0, n_columns)
-for (i in 1:n_columns){
-  column_nas[i] <- sum(is.na(data[,i]))
+to_date_in_months <- function(x){
+  as.numeric(unlist(x))[1] * 12 + as.numeric(unlist(x))[2]
 }
-column_nas #number of NAs per column
-column_nas / n_rows #percentage of NAs per column
 
-sum(!complete.cases(data))
-row_nas #number of rows w/ NAs
-row_nas / n_rows #percentage of rows w/ NAs
-
+data$AVERAGE.ACCT.AGE <- lapply(data$AVERAGE.ACCT.AGE, to_date_in_months)
